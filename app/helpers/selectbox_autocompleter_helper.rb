46a822262f9962da354f90661d8f837f7df566c5
Roman Petrenko <roman.l.petrenko@gmail.com>
2024-09-09 22:40:00 +0300

    feat: progress

--- app/helpers/selectbox_autocompleter_helper.rb
+++ app/helpers/selectbox_autocompleter_helper.rb
@@ -6,7 +6,20 @@ module SelectboxAutocompleterHelper
     return "[]" if target_list.blank?
     
     joind_target = target_list.split.map{ |line| "'#{j(line.chomp)}'" }.join(', ')
-    generate_array = "[#{joind_target}]"
+    "[#{joind_target}]"
+  end
+
+  def generate_exclude_target_list_for_js
+    target_list =  Setting.plugin_selectbox_autocompleter['exclude_target_list']
+
+    return "[]" if target_list.blank?
+
+    exclude_list = target_list.split.map{ |line| "'#{j(line.chomp)}'" }.join(', ')
+    "[#{exclude_list}]"
+  end
+
+  def generate_autocompleter_min_count_show_search
+    Setting.plugin_selectbox_autocompleter['min_count_show_search']
   end
 
 end
